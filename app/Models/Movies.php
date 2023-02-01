db7a7d57d25b6818a6ff3a19c214eb52e06a361f
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-01 15:00:48 +0100

    Collegamento Movies per poterlo passare a view

---
+++ app/Models/Movies.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movies extends Model
+{
+    use HasFactory;
+
+    private $title;
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+}
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main-layout')
+
+@section('main')
+    <h1>hello</h1>
+@endsection
